f4e4d29c806415d45897de0a9404d9f7a754e042
sandeeprawat4 <sandeep.rawat4@gmail.com>
2022-06-05 19:28:55 +0530

    Created Trie Structure

--- src/main/java/com/sr/model/trie/ContactTrie.java
+++ src/main/java/com/sr/model/trie/ContactTrie.java
@@ -2,4 +2,17 @@ package com.sr.model.trie;
 
 public class ContactTrie {
     TrieNode root;
+    public ContactTrie(){
+        this.root = new TrieNode();
+    }
+    public void insert(String name){
+        TrieNode node = root;
+        for(Character c : name.toCharArray()){
+            if(!node.trie.containsKey(c)){
+                node.trie.put(c, new TrieNode());
+            }else
+                node = node.trie.get(c);
+        }
+        node.end = true;
+    }
 }
